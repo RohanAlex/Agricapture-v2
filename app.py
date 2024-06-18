a71d4a12b32e7bcb6afd251161027849abe2cff7
RohanAlex <rohanalex2002@gmail.com>
2024-06-19 01:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ def display_realtime_data():
             if key=='SoilMoisture':
                 if data[key]==100:
                     send_email(body_flood)
-                elif data[key]<=20:
+                elif data[key]<=3:
                     send_email(body_water)
                 else:
                 
